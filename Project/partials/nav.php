371b333cd60097e628556aa7c9466870c544391b
shahbajsingh <ss4589@njit.edu>
2024-07-28 21:21:37 -0400

    misc.

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -1,24 +1,25 @@
 <?php
+session_start();
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
-$localWorks = true; //some people have issues with localhost for the cookie params
+$localWorks = false; //some people have issues with localhost for the cookie params
 //if you're one of those people make this false
 
 //this is an extra condition added to "resolve" the localhost issue for the session cookie
-if (($localWorks && $domain == "localhost") || $domain != "localhost") {
-    session_set_cookie_params([
-        "lifetime" => 60 * 60,
-        "path" => "/Project",
-        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
-        "domain" => $domain,
-        "secure" => true,
-        "httponly" => true,
-        "samesite" => "lax"
-    ]);
-}
+// if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+//     session_set_cookie_params([
+//         "lifetime" => 60 * 60,
+//         "path" => "/Project",
+//         // "domain" => $_SERVER["HTTP_HOST"] || "localhost",
+//         "domain" => $domain,
+//         "secure" => true,
+//         "httponly" => true,
+//         "samesite" => "lax"
+//     ]);
+// }
 require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
@@ -26,6 +27,10 @@ require_once(__DIR__ . "/../lib/functions.php");
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <?php if (has_role('admin')) : ?>
+                <li><a href="admin.php">Admin</a></li>
+            <?php endif; ?>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
